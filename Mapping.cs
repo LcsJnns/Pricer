291448306df11e84901f82bbb5b86c2dc011b67d
LcsJnns <Lucas.jannessup@gmail.com>
2023-02-10 17:39:04 +0100

    10022023

---
+++ Mapping.cs
@@ -0,0 +1,39 @@
+﻿using Deserialization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YahooAPI;
+
+namespace Map
+{
+    public class Mapping
+    {
+        public double stockPrice;
+        public double strike;
+        public double expirationTime;
+        public double volatility;
+
+        public Mapping()
+        {
+            Task.Run(async () =>
+            {
+                DataMarket datamarket = new DataMarket();
+                Root Myroot = await datamarket.JSONtoclass();
+
+                stockPrice = Myroot.optionChain.result[0].options[0].calls[0].lastPrice;
+                strike = Myroot.optionChain.result[0].options[0].calls[0].strike;
+                double tempExpirationTime = Myroot.optionChain.result[0].options[0].calls[0].expiration;
+                volatility = Myroot.optionChain.result[0].options[0].calls[0].impliedVolatility;
+
+                DateTime today = DateTime.Now;
+                double timestamp = (long)(today - new DateTime(1970, 1, 1)).TotalSeconds;
+                tempExpirationTime -= timestamp;
+                expirationTime = tempExpirationTime / 31536000;
+
+
+            }).Wait();
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -10,7 +10,9 @@ using YahooAPI;
 using Newtonsoft.Json.Linq;
 using Newtonsoft.Json;
 using Deserialization;
+using System.Drawing;
 using Graphic;
+using Map;
 
 namespace PricerProject
 {
@@ -20,41 +22,18 @@ namespace PricerProject
         {
 
 
-            ////////////////////////////////////////
+            Mapping mapping = new Mapping();
+            Console.WriteLine("Stock Price : " + mapping.stockPrice);
+            Console.WriteLine("strike : " + mapping.strike);
+            Console.WriteLine("expiration date : " + mapping.expirationTime);
+            Console.WriteLine("volatility : " + mapping.volatility);
 
-            // API Yahoo finance va donner ces valeurs
-            double CoursAction = 120;
-            double Strike = 120;
-            double TauxSansRisque = 0.01;
-            double Volatility = 0.3;
-            double expirationTime = 1;
-            // 
-
-            double CallPrice = Black_Scholes.Call_Pricing(CoursAction, Strike, expirationTime, TauxSansRisque, Volatility);
-            //Console.WriteLine($"Call price = {CallPrice}");
-
-            double PutPrice = Black_Scholes.Put_Pricing(CoursAction, Strike, expirationTime, TauxSansRisque, Volatility);
-            //Console.WriteLine("Put price = " + PutPrice);
-
-            // Deserialisation du JSON
-            DataMarket datamarket = new DataMarket();
-            Root Myroot = await datamarket.JSONtoclass();
-
-            CoursAction = Myroot.optionChain.result[0].options[0].calls[0].lastPrice;
-            Strike = Myroot.optionChain.result[0].options[0].calls[0].strike;
-            expirationTime = Myroot.optionChain.result[0].options[0].calls[0].expiration;
-            Volatility = Myroot.optionChain.result[0].options[0].calls[0].impliedVolatility;
-
-            Console.WriteLine(CoursAction);
-            Console.WriteLine(Strike);
-            Console.WriteLine(expirationTime);
-            Console.WriteLine(Volatility);
-
-            CallPrice = Black_Scholes.Call_Pricing(CoursAction, Strike, 0.5, TauxSansRisque, Volatility);
+            double CallPrice = Black_Scholes.Call_Pricing(mapping.stockPrice, mapping.strike, mapping.expirationTime, 0.03, mapping.volatility);
             Console.WriteLine($"Call price = {CallPrice}");
 
             Graphe gc = new Graphe();
             gc.CreateGraphe();
+
 
 
         }
